34a82f1ab149c7e617700c5df9242864385802cb
lib-Pluton <45429108+lib-Pluton@users.noreply.github.com>
2021-01-17 23:18:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ assets : {
 large_image : "Big Picture File Name on Discord Dev Portal", // You added one file on Discord Dev Portal, write this file name
 large_text : "Big Picture View Name on Discord" // When your cursor on this image you can see this text.
 },
-buttons : [{label : "Application Name" , url : "Website Link"},{label : "Application Name", url : "Website link"}] // Example : "Application name = Github" "Website Link : https://github.com/lib-Pluton  
-}
+buttons : [{label : "Application Name" , url : "Website Link"},{label : "Application Name", url : "Website link"}]
+}  // Example : "Application name = Github" "Website Link : https://github.com/lib-Pluton  
 })
 })
 client.login({ clientId : "Developer App ID" }).catch(console.error); // Your Application ID on Discord Dev Portal
