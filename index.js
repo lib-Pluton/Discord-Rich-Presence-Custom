75676896aa224842e5f69c23addef28503a9fec4
lib-Pluton <45429108+lib-Pluton@users.noreply.github.com>
2021-01-17 23:08:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ client.on('ready', () => {
 client.request('SET_ACTIVITY', {
 pid: process.pid,
 activity : {
-details : "Doing Something But Who Knows?", 
+details : "Details side", // You can write everything, like my text is "Doing Something But Who Knows?"
 assets : {
-large_image : "hello",
-large_text : "Kali Linux"
+large_image : "Big Picture File Name on Discord Dev Portal", // You added one file on Discord Dev Portal, write this file name
+large_text : "Big Picture View Name on Discord" // When your cursor on this image you can see this text.
 },
-buttons : [{label : "Twitch" , url : "https://twitch.tv/plutonlib"},{label : "GitHub", url : "https://github.com/lib-Pluton"}]
+buttons : [{label : "Application Name" , url : "Website Link"},{label : "Application Name", url : "Website link"}] // Example : "Application name = Github" "Website Link : https://github.com/lib-Pluton  
 }
 })
 })
-client.login({ clientId : "800260589355270155" }).catch(console.error);
+client.login({ clientId : "Developer App ID" }).catch(console.error); // Your Application ID on Discord Dev Portal
